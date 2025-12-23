1d495efc39e653afe79f251d6c3b6d9a08f2ee23
jhapriyanshu16 <priyanshu2015madhepur@gmail.com>
2025-12-23 16:44:17 +0530

    parametarized methods

---
+++ coreJavaCapgemini/basics/methods/MethodQ2.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class MethodQ2 {
+	public static int add (int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+	public static void main(String[] args) {
+		 int s = add(3,5);
+		 System.out.println(s);
+	}
+
+}
